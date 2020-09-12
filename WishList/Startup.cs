eba43f511a062dda306277dc367d63abd184b2c2
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-13 01:22:46 +0300

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -10,11 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 namespace WishList
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+     public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
@@ -22,10 +18,11 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.Run(async (context) =>
             if (env.IsDevelopment())
             {
-               
-           
+                await context.Response.WriteAsync("Hello World!");
+            });
                 app.UseDeveloperExceptionPage();
             }
             else
