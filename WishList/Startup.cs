199d462721123ca42a7b582f792baf529e86bf3f
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-13 02:21:21 +0300

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -20,7 +20,7 @@ namespace WishList
         {
             services.AddMvc();
          
-            );
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
