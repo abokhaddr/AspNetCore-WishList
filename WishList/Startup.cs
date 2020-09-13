831e7d09c08f375432acbc7ab81801a680748849
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-13 23:52:14 +0300

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -18,10 +18,21 @@ namespace WishList
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(
+                endpoints => { endpoints.MapDefaultControllerRoute(); }  );
+        }  
         }
     }
 }
