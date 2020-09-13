f07139bceff7baffe48436c4b67d8e3d43c3dacd
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-14 00:56:17 +0300

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -19,14 +19,7 @@ namespace WishList
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-           if(env.IsDevelopment())
-           {
-               app.UseDeveloperExceptionPage();
-           }   
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+          
             app.UseRouting();
             app.UseEndpoints(
                 endpoints => { endpoints.MapDefaultControllerRoute(); }
