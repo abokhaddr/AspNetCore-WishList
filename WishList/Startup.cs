4e4e7d132f5131a320ee3f5b25285179f24f8bd2
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-13 23:13:14 +0300

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -14,15 +14,11 @@ namespace WishList
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();   
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-       
-    
     }
 }
