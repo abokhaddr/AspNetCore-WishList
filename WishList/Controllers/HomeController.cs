a9e802f79ecb787bc693d575a7b27e267eb17a06
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-14 01:33:50 +0300

    Update HomeController.cs

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -3,10 +3,14 @@ namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
-
         public IActionResult Index()
         {
             return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
         }
     }
 }
