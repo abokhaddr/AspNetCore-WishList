00227a9c8cc1d42c1a86b8114b35df8de2516dcf
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-14 02:09:11 +0300

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -1,14 +1,11 @@
+ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Data;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-
 namespace WishList.Controllers
 {
     public class ItemController : Controller
     {
         private readonly ApplicationDbContext _context;
-
         public ItemController(ApplicationDbContext context)
         {
             _context = context;
@@ -18,26 +15,25 @@ namespace WishList.Controllers
             var model = _context.Items.ToList();
             return View("Index", model);
         }
-        
         [HttpGet]
         public IActionResult Create()
         {
             return View("Create");
         }
-          [HttpPost]
+        [HttpPost]
         public IActionResult Create(Models.Item item)
         {
             _context.Items.Add(item);
             _context.SaveChanges();
-            return RedirectToAction("Index"
-        } 
-         public IActionResult Delete(int id)
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
         {
             var item = _context.Items.FirstOrDefault(e => e.Id == id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
-           
     }
 }
