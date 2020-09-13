0ca3ae8c9cdba30c2b05f9798875492b7c03f036
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-14 00:07:06 +0300

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -18,7 +18,7 @@ namespace WishList
         
 
             app.UseRouting();
-            app.UseEndpoints( endpoints => { endpoints.MapDefaultControllerRoute(); });
+            
         }
     }
 }
