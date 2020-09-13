145c5e3aaaba78d5b7e6f477f836632bfa219b95
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-13 23:50:45 +0300

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -7,9 +7,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using WishList.Data;
-
 namespace WishList
 {
     public class Startup
@@ -18,8 +15,13 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();   
+            services.AddMvc();
         }
 
-   
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            
+        }
+    }
 }
