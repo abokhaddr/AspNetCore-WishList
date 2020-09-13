8988182848683fbda9049192c61f9eef3100d377
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-14 00:14:38 +0300

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -16,5 +16,21 @@ namespace WishList
             services.AddMvc();
         
         }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(
+                endpoints => { endpoints.MapDefaultControllerRoute(); }
+            );
+        }
     }
 }
